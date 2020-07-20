75e700c824fbab9dbae17aff234f35425ea889b4
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 18:16:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,5 @@ def reverse_each_word(sentence)
     reverse_ordered_words.unshift(element)
   end
   
-  reversed_sentence = ""
-  reverse_ordered_words.each do |element|
-    reversed_sentence = reversed_ordered_sentence + " "
-  end
+  reversed_sentence = reverse_ordered_words.join()
 end
