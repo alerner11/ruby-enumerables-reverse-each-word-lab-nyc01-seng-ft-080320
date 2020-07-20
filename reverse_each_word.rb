69c7336d6a13a910e48228aa8d6a1d936fca406d
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-20 18:23:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   ordered_words = sentence.split
-  ordered_words.collect do |index|
-    reverse_ordered_words << orig_ordered_words[index].reverse
+  ordered_words.collect do |word|
+    word.reverse
   end
   
   reverse_ordered_words.join(" ")
